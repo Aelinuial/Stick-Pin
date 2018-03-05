e6f1fa400cf135c5ea7b49902e9563911de8931c
长歌承闲 <aelinuial@163.com>
2018-03-05 16:03:40 +0800

    first commit

---
+++ Assets/Script/Manager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour {
+
+    private bool isGameOver = false;
+    public Spawner spawner;
+    public Rotater rotater;
+
+	// Use this for initialization
+	void Start () {
+        Screen.SetResolution(450, 750, false);
+    }
+	
+    public void GameOver()
+    {
+        if (!isGameOver)
+        {
+            isGameOver = true;
+            spawner.enabled = false;
+            rotater.enabled = false;
+            GetComponent<Animator>().SetTrigger("Gameover");
+            Debug.Log("Game Over");
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/Pin.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+    public float speed = 20f;
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.up * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Rotater")
+        {
+            rb.velocity = Vector2.zero;
+            if (Random.Range(0f, 1f) > 0.5f)
+            {
+                collision.GetComponent<Rotater>().speed *= -1;
+            }
+            collision.GetComponent<Rotater>().speed += 10;
+            Score.scoreValue++;
+            transform.SetParent(collision.transform);
+        }else if (collision.tag == "Pin")
+        {
+            GameObject.FindObjectOfType<Manager>().GameOver();
+        }
+    }
+}
